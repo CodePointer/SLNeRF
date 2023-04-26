Fix mask loading bug & gradient bugs.

dataset/multi_pat_dataset.py
@@ -260,8 +260,9 @@ class PatchLCNDataset(torch.utils.data.Dataset):
         # Get mask
         self.mat_set['mask'] = torch.zeros([1, self.img_size[1], self.img_size[0]], dtype=torch.float)
         self.mat_set['mask'][:, rad:-rad, rad:-rad] = 1.0
-        if (scene_folder / 'mask' / 'mask_occ.png').exists():
-            self.mat_set['mask'] *= plb.imload(scene_folder / 'mask' / 'mask_occ.png')
+        if (scene_folder / 'gt' / 'mask_occ.png').exists():
+            self.mat_set['mask'] *= plb.imload(scene_folder / 'gt' / 'mask_occ.png')
+        self.valid_coord = torch.stack(torch.where(self.mat_set['mask'][0] > 0), dim=0)
 
         self.patch_set = {key: mat2patch(self.mat_set[key], self.rad) for key in ['img', 'mask']}
 
@@ -345,12 +346,15 @@ class PatchLCNDataset(torch.utils.data.Dataset):
         """
         Generate random rays at world space from one camera.
         """
-        pixels_x = torch.randint(low=self.wid_range[0],
-                                 high=self.wid_range[1],
-                                 size=[self.sample_num])
-        pixels_y = torch.randint(low=self.hei_range[0],
-                                 high=self.hei_range[1],
-                                 size=[self.sample_num])
+        coord_idx = torch.randint(low=0, high=self.valid_coord.shape[1], size=[self.sample_num])
+        pixels_y = self.valid_coord[0, coord_idx]
+        pixels_x = self.valid_coord[1, coord_idx]
+        # pixels_x = torch.randint(low=self.wid_range[0],
+        #                          high=self.wid_range[1],
+        #                          size=[self.sample_num])
+        # pixels_y = torch.randint(low=self.hei_range[0],
+        #                          high=self.hei_range[1],
+        #                          size=[self.sample_num])
 
         color = self.mat_set['img'][:, pixels_y, pixels_x].permute(1, 0)  # [batch_size, C]
         mask = self.mat_set['mask'][:, pixels_y, pixels_x].permute(1, 0)  # [batch_size, 1]
@@ -408,7 +412,8 @@ class PatchLCNDataset(torch.utils.data.Dataset):
         dist = torch.sqrt(xx.float() ** 2 + yy.float() ** 2)
         gaussian_val = torch.exp(- dist ** 2 / sigma ** 2)
 
-        kernel = gaussian_val / gaussian_val.sum()
+        # kernel = gaussian_val / gaussian_val.sum()   This may cause numerical problems.
+        kernel = gaussian_val
         return kernel.reshape(1, -1)  # [1, pch_len ** 2]
 
     def pixel2ray(self, pixels_x, pixels_y):

networks/renderer.py
@@ -70,7 +70,7 @@ class NeuSRendererPatch(NeuSRenderer):
             #       pts_nbr = pts_o + t * v_vec
             #       t = (pts_cen - pts_o) * n_vec / v_vec * n_vec
             # Dimension: [batch_num, n_samples, dim (3), pch_len^2].
-            n_vec = gradients.reshape(batch_size, n_samples, 3, 1)
+            n_vec = gradients.reshape(batch_size, n_samples, 3, 1).detach()
             pts_cen = pts.reshape(batch_size, n_samples, 3, 1)
             pts_o = rays_o.reshape(batch_size, 1, 3, 1)
             v_vec = rays_d_patch.reshape(batch_size, 1, 3, -1)
@@ -168,7 +168,6 @@ class NeuSRendererPatch(NeuSRenderer):
             with torch.no_grad():
                 pts = rays_o[:, None, :] + rays_d[:, None, :] * z_vals[..., :, None]
                 sdf = self.sdf_network.sdf(pts.reshape(-1, 3)).reshape(batch_size, self.n_samples)
-
                 for i in range(self.up_sample_steps):
                     new_z_vals = self.up_sample(rays_o,
                                                 rays_d,